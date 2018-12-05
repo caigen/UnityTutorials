1c4b3918950b296953d36392a8a8fb55b03be58e
caigen <caigen@outlook.com>
2018-12-05 15:58:38 +0800

    Multiplayer Networking Tutorial.

---
+++ Multiplayer Networking/Assets/Health.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+    public const int maxHealth = 100;
+
+    public bool destroyOnDeath;
+
+    [SyncVar(hook ="OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    private NetworkStartPosition[] spawnPoints;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = maxHealth;
+
+                // called on the Server, but invoked on the Clients
+                RpcRespawn();
+            }
+        }
+    }
+
+    void OnChangeHealth(int health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // Set the spawn point to origin as a default value
+            Vector3 spawnPoint = Vector3.zero;
+
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            transform.position = spawnPoint;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	    if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
