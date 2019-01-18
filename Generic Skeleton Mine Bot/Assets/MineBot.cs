e547145ac3c8393d2cc3e141f38dde12a138dcf7
caigen <caigen@outlook.com>
2019-01-18 16:55:10 +0800

    Generic Skeleton Example.

---
+++ Generic Skeleton Mine Bot/Assets/MineBot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineBot : MonoBehaviour {
+
+    private Animator animator;
+    private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (animator)
+        {
+            bool j = Input.GetButton("Fire1");
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", Mathf.Atan2(h, v) * 180.0f / Mathf.PI);
+            animator.SetBool("Jump", j);
+
+            if (rigidbody)
+            {
+                Vector3 speed = rigidbody.velocity;
+                speed.x = 4 * h;
+                speed.z = 4 * v;
+                rigidbody.velocity = speed;
+                if (j)
+                {
+                    rigidbody.AddForce(Vector3.up * 20);
+                }
+            }
+        }		
+	}
+}
---
+++ Generic Skeleton Mine Bot/Assets/MineBotCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineBotCamera : MonoBehaviour {
+    public float distanceAway;
+    public float distanceUp;
+    public float smooth;
+
+    private Transform follow;
+    private Vector3 targetPosition;
+    
+	// Use this for initialization
+	void Start () {
+        follow = GameObject.FindWithTag("Player").transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetPosition = follow.position + Vector3.up * distanceUp + follow.up * distanceAway;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+
+        transform.LookAt(follow);
+	}
+}
